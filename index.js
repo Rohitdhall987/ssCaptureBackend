4be43ce9cb7ac010ea59a3986ababb06e6c750f9
Rohit <72535349+Rohitdhall987@users.noreply.github.com>
2025-12-07 10:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ wss.on('connection', (ws) => {
         // const imageBuffer = Uint8Array.from(Buffer.from(imageBase64, 'base64'));
 
         const model = genAI.getGenerativeModel({
-          model: 'gemini-1.5-flash',
+          model: 'gemini-1.5-flash-latest',
         });
 
         const result = await model.generateContent([
